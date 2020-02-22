04d8f277193a8ce80a706e7d629d12b9c5d1a2c4
Zack Lee <cuinjune@gmail.com>
2020-02-22 13:29:42 -0500

    added submit button

--- main.js
+++ main.js
@@ -1,47 +1,79 @@
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-  
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-  
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-  
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-    return array;
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
   }
+  return array;
+}
 
 window.addEventListener('DOMContentLoaded', async () => {
 
-    const imageUrl = "https://dog.ceo/api/breeds/image/random";
-    const imageUrlApiOptions = {method:"GET"};
-    const imageUrlResponse = await fetch(imageUrl, imageUrlApiOptions);
-    const imageJson = await imageUrlResponse.json();
-    const $img = document.getElementById("dogImage");
-    $img.src = imageJson.message;
+  //get a random breed dog image source
+  const imageUrl = "https://dog.ceo/api/breeds/image/random";
+  const imageUrlApiOptions = { method: "GET" };
+  const imageUrlResponse = await fetch(imageUrl, imageUrlApiOptions);
+  const imageJson = await imageUrlResponse.json();
+  const $img = document.getElementById("dogImage");
+  $img.src = imageJson.message;
 
-    //get the breed of the dog in the photo
-    const imageJsonMessage = imageJson.message;
-    const lastSlashIndex = imageJsonMessage.lastIndexOf('/');
-    const breedStr = imageJsonMessage.slice(30, lastSlashIndex);
-    console.log(breedStr);
+  //get the breed of the dog in the photo
+  const imageJsonMessage = imageJson.message;
+  const lastSlashIndex = imageJsonMessage.lastIndexOf('/');
+  const breedStr = imageJsonMessage.slice(30, lastSlashIndex);
+  console.log(breedStr);
 
-    //get the full list of breeds
-    const breedUrl = "https://dog.ceo/api/breeds/list/all";
-    const breedUrlApiOptions = {method:"GET"};
-    const breedUrlResponse = await fetch(breedUrl, breedUrlApiOptions);
-    const breedJson =  await breedUrlResponse.json();
-    const breedList = Object.keys(breedJson.message);
+  //get the full list of breeds
+  const breedUrl = "https://dog.ceo/api/breeds/list/all";
+  const breedUrlApiOptions = { method: "GET" };
+  const breedUrlResponse = await fetch(breedUrl, breedUrlApiOptions);
+  const breedJson = await breedUrlResponse.json();
+  const breedList = Object.keys(breedJson.message);
 
-    //make selectable list of breeds
-    shuffle(breedList);
-    breedList.length = 4;
-    breedList.push(breedStr);
-    shuffle(breedList);
-    console.log(breedList);
+  //make selectable list of breeds
+  shuffle(breedList);
+  const numOptions = 3;
+  breedList.length = numOptions - 1;
+  breedList.push(breedStr);
+  shuffle(breedList);
+  console.log(breedList);
+  const $breeds = document.getElementsByName("breed");
+  const $breedLabels = document.getElementsByName("breed__label");
+
+  for (let i = 0; i < breedList.length; ++i)
+  {
+    $breeds[i].value = breedList[i];
+    $breedLabels[i].textContent = breedList[i];
+  }
+    
+  //breed button listener
+  const $breedButton = document.querySelector(".breed__button");
+  $breedButton.addEventListener('click', (out) => {
+
+    for (let i = 0; i < $breeds.length; ++i)
+    {
+      if ($breeds[i].checked)
+      {
+        if ($breeds[i].value == breedStr)
+        {
+          console.log("CORRECT");
+        }
+        else
+        {
+          console.log("WRONG");
+        }
+        break;
+      }
+    }
+  });
+
 });
